da39960eeffb7deb510d022728965f95e8fe1c30
abtheo <c.theo.kent@gmail.com>
2017-03-18 23:57:13 +0000

    v2.0 branch
    Version 2.0, improving object argument handling

---
+++ Smartbell_v2.0.py
@@ -0,0 +1,65 @@
+#Smartbell
+#Program to handle JSON data
+#Basically, a practice framework for talking to the web server
+#Using example data from 'Databae'
+
+#Imported libraries
+import json
+
+#Object handling JSON input data
+class jsonData(object):
+    #Descriptor Definitions:
+    #Instantiation
+    def __init__(self, *args):
+        if (len(args) > 0):
+            self.data = json.loads(','.join(x for x in args))
+        else:
+            self.data = []
+    #Sets data to input list data
+    def __set__(self, dataList):
+        self.data = dataList
+    #Gets data from class as list
+    def __get__(self):
+        return self.data
+    #Deletes current data
+    def __del__(self):
+        del self.data
+
+    #Getters / Setters:
+    #Gets all values of a key
+    def get_byKey(self, key):
+        return ','.join([x[key] for x in self.data])
+  
+
+    #dump utility; printing:
+    #Prints all data
+    def dump(self, *args):
+        for x in self.data:
+            print(x)
+    #Prints all instances of key
+    def dump(self, key):
+        for x in self.data:
+            print(x[key])
+
+    #search utility; basic searching:
+    #Returns list of instances from key and query
+    def search(self, key, query):
+        resultList = []
+        for x in self.data:
+            if (x[key] == query):
+                resultList.append(x)
+        return resultList
+
+#Example data    
+Databae_data = '[{"name":"Carl","age":28,"city":"Ipswich"},{"name":"Kevin","age":28,"city":"Ipswich"},{"name":"Owen","age":28,"city":"Ipswich"},{"name":"Ryan","age":28,"city":"Ipswich"}]'
+
+#Examples of using and handling:
+#Creates object holding example JSON data
+myObject = jsonData(Databae_data)
+#Search
+print(myObject.search("name", "Carl"))
+#Creates new empty object
+newObject = jsonData()
+
+newObject.dump()
+
