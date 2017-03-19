ece1e2d80729b897129580aaf98e013c43345848
abtheo <c.theo.kent@gmail.com>
2017-03-19 02:06:47 +0000

    more utility coming up

--- Smartbell_v2.1.py
+++ Smartbell_v2.1.py
@@ -12,7 +12,7 @@ class jsonData(object):
     #Instantiated as JSON list or empty
     def __init__(self, *args):
         if args:
-            self.data = json.loads(','.join(x for x in args))
+            self.data = json.loads(','.join(str(x) for x in args))
         else:
             self.data = []
     #Sets data to input list data
@@ -29,7 +29,10 @@ class jsonData(object):
     #Gets all values of a key
     def get_byKey(self, key):
         return ','.join([x[key] for x in self.data])
-  
+    #Sets data in JSON format; takes list
+    #WILL NEED ERROR CHECKING
+    def set_listToJson(self, dataList):
+        self.data = json.loads(json.dumps(dataList))
 
     #dump utility; printing:
     def dump(self, *args):
@@ -43,25 +46,34 @@ class jsonData(object):
                 print(x)
 
     #search utility; basic searching:
-    #Returns list of instances by key or query
-    def search(self, *args):
+    #Returns list of results by key and/or query
+    def search(self, key, value):
         resultList = []
-        if args:
-            for x in self.data:
-                if (x[args[0]] == args[1]):
-                    resultList.append(x)
+        for x in self.data:
+            if (x[key] == value):
+                resultList.append(x)
         return resultList
 
+    #filter utility; essentially layered basic searching:
+    #Takes keyvalue pair and optional bool arg for OR / AND filter (Default OR)
+    #Returns list of results
+    def filter(self, **qargs, layer=false):
+        resultList = []
+        if qargs:
+            for x in self.data:
+                for key, value in qargs.items():
+                    if (x[str(key)] == value):
+                        resultList.append(x)
+        return resultList
+    #Returns list of results of keyvalue pairs
+    
+
 #Example data    
-Databae_data = '[{"name":"Carl","age":28,"city":"Ipswich"},{"name":"Kevin","age":28,"city":"Ipswich"},{"name":"Owen","age":28,"city":"Ipswich"},{"name":"Ryan","age":28,"city":"Ipswich"}]'
+Databae_data = '[{"name":"Carl","age":34,"city":"Ipswich"},{"name":"Carl","age":28,"city":"Ipswich"},{"name":"Kevin","age":28,"city":"Ipswich"},{"name":"Owen","age":28,"city":"Ipswich"},{"name":"Ryan","age":28,"city":"Ipswich"}]'
 
 #Examples of using and handling:
 #Creates object holding example JSON data
 myObject = jsonData(Databae_data)
 #Search
-print(myObject.search("name","Carl"))
-#Creates new empty object
-newObject = jsonData()
-
-#myObject.dump("name")
+print(myObject.search("age",34))
 
