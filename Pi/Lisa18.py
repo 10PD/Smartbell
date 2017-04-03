b3a76905dcb9d020f525566f52abe7ab73677d9d
abtheo <c.theo.kent@gmail.com>
2017-04-03 18:17:09 +0100

    Lisa18 update2

--- Pi/Lisa18.py
+++ Pi/Lisa18.py
@@ -2,16 +2,18 @@
 import LIS3DH as Lisa
 from time import sleep
 
-print("Trying to initialise Lisa18...")
-Lisa18 = initLisa(0x18, -1)
-Lisa19 = initLisa(0x19, -0)
-
 def initLisa(add, bus):
+    print("Trying to initialise Lisa%x..." % add)
     try:
         newLisa = Lisa.LIS3DH(add, bus)
         newLisa.setRange(Lisa.LIS3DH.RANGE_2G)
     except IOError:
         print("Failed to initialise Lisa%x" % add)
+
+
+
+Lisa18 = initLisa(0x18, -1)
+Lisa19 = initLisa(0x19, -0)
 
 print ("Starting stream")
 while True:
