277f1d9d0f1480dc74604f1c49c1b41d74223d39
abtheo <c.theo.kent@gmail.com>
2017-04-03 19:02:07 +0100

    Accelerometer remade, avg added

--- Pi/Accelerometer.py
+++ Pi/Accelerometer.py
@@ -1,38 +1,30 @@
 #Program to get Lisa18's data and interpreit
+#AVERAGES:
+xAvg: -0.152029
+yAvg: 0.291956
+zAvg: 0.960752
 import LIS3DH as Lisa
 from time import sleep
 
 Lisa18 = Lisa.LIS3DH(0x18, -1)
 Lisa18.setRange(Lisa.LIS3DH.RANGE_2G)
 
-#Loop stuff
-xList = []
-yList = []
-zList = []
-i = 0
-
-def avg(dataList):
-    temp = 0
-    for data in dataList:
-        temp += data
-    temp = temp / len(dataList)
-    return temp
-
 print ("Starting stream")
-while i < 100:
+while True:
     
     x = Lisa18.getX()
     y = Lisa18.getY()
     z = Lisa18.getZ()
 
+    x_acc = (x - xAvg)
+    y_acc = (y - yAvg)
+    z_acc = (z - zAvg)
+    
 #raw values
-    print(("\rX: %.6f\tY: %.6f\tZ: %.6f") % (x,y,z))
+    print(("\rX: %.6f\tY: %.6f\tZ: %.6f") % (x_acc,y_acc,z_acc))
     
     xList.append(x)
     yList.append(y)
     zList.append(z)
     #Is this the hz we want??
-    i += 1
     sleep(0.1)
-
-print(("\r\nAVERAGES: X: %.6f\tY: %.6f\tZ: %.6f") % ( avg(xList) , avg(yList) , avg(zList) ))
---
+++ Pi/Average.py
@@ -0,0 +1,42 @@
+#Program to get Lisa18's data and interpreit
+#AVERAGES:
+#X: -0.152029
+#Y: 0.291956
+#Z: 0.960752
+import LIS3DH as Lisa
+from time import sleep
+
+Lisa18 = Lisa.LIS3DH(0x18, -1)
+Lisa18.setRange(Lisa.LIS3DH.RANGE_2G)
+
+#Loop stuff
+xList = []
+yList = []
+zList = []
+i = 0
+
+def avg(dataList):
+    temp = 0
+    for data in dataList:
+        temp += data
+    temp = temp / len(dataList)
+    return temp
+
+print ("Starting stream")
+while i < 100:
+    
+    x = Lisa18.getX()
+    y = Lisa18.getY()
+    z = Lisa18.getZ()
+
+#raw values
+    print(("\rX: %.6f\tY: %.6f\tZ: %.6f") % (x,y,z))
+    
+    xList.append(x)
+    yList.append(y)
+    zList.append(z)
+    #Is this the hz we want??
+    i += 1
+    sleep(0.1)
+
+print(("\r\nAVERAGES: X: %.6f\tY: %.6f\tZ: %.6f") % ( avg(xList) , avg(yList) , avg(zList) ))
