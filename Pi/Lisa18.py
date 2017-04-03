9f6d81db2c54f1a257397e2faa0a7acb31347fab
abtheo <c.theo.kent@gmail.com>
2017-04-03 18:20:41 +0100

    Lisa18 update3

--- Pi/Lisa18.py
+++ Pi/Lisa18.py
@@ -2,14 +2,18 @@
 import LIS3DH as Lisa
 from time import sleep
 
+#Takes memory address (0x18 / 0x19)
+#Takes bus (-1 / -0)
 def initLisa(add, bus):
     print("Trying to initialise Lisa%x..." % add)
     try:
         newLisa = Lisa.LIS3DH(add, bus)
         newLisa.setRange(Lisa.LIS3DH.RANGE_2G)
+        print("Lisa%x initialised!" % add)
+        return newLisa
     except IOError:
         print("Failed to initialise Lisa%x" % add)
-
+    
 
 
 Lisa18 = initLisa(0x18, -1)
