9f455be8c7c224c3466de263f72e153c3f2d5e88
abtheo <c.theo.kent@gmail.com>
2017-04-21 16:26:01 +0100

    Rep.py

--- Pi/Rep.py
+++ Pi/Rep.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-
+#Program to give a TRUE to a BCM Pin
+#Triggered when system is not moving for time
 import smbus
 import math
 import time
@@ -70,8 +71,10 @@ gyro_total_y = (last_y) - gyro_offset_y
 
 print "{0:.4f} {1:.2f} {2:.2f} {3:.2f} {4:.2f} {5:.2f} {6:.2f}".format( time.time() - now, (last_x), gyro_total_x, (last_x), (last_y), gyro_total_y, (last_y))
 
-#3.0 / time_diff = samples
-for i in range(0, int(3.0 / time_diff)):
+
+#1.5 / time_diff = samples
+int repCalc = 0
+for i in range(0, int(6 / time_diff)):
     time.sleep(time_diff - 0.005) 
     
     (gyro_scaled_x, gyro_scaled_y, gyro_scaled_z, accel_scaled_x, accel_scaled_y, accel_scaled_z) = read_all()
@@ -90,5 +93,11 @@ for i in range(0, int(3.0 / time_diff)):
 
     last_x = K * (last_x + gyro_x_delta) + (K1 * rotation_x)
     last_y = K * (last_y + gyro_y_delta) + (K1 * rotation_y)
-    
+    repCalc = repCalc + last_x + last_y)
     print "{0:.4f} {1:.2f} {2:.2f} {3:.2f} {4:.2f} {5:.2f} {6:.2f}".format( time.time() - now, (rotation_x), (gyro_total_x), (last_x), (rotation_y), (gyro_total_y), (last_y))
+    with open("output.txt", "a") as myfile:
+    myfile.write("appended text")
+
+
+
+    
