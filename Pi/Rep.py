040d965363f4cf42722c46a4a88ffbcfe499f244
abtheo <c.theo.kent@gmail.com>
2017-05-01 17:20:54 +0100

    Updated rep calc

--- Pi/Rep.py
+++ Pi/Rep.py
@@ -46,38 +46,45 @@ def get_x_rotation(x,y,z):
     radians = math.atan2(y, dist(x,z))
     return math.degrees(radians)
 
-bus = smbus.SMBus(1)  # or bus = smbus.SMBus(1) for Revision 2 boards
+#Sets the plane of orientation
+def initPlane():
+    (gyro_scaled_x, gyro_scaled_y, gyro_scaled_z, accel_scaled_x, accel_scaled_y, accel_scaled_z) = read_all()
 
+    last_x = get_x_rotation(accel_scaled_x, accel_scaled_y, accel_scaled_z)
+    last_y = get_y_rotation(accel_scaled_x, accel_scaled_y, accel_scaled_z)
+
+    gyro_offset_x = gyro_scaled_x 
+    gyro_offset_y = gyro_scaled_y
+
+    gyro_total_x = (last_x) - gyro_offset_x
+    gyro_total_y = (last_y) - gyro_offset_y
+
+    #print ("{0:.4f} {1:.2f} {2:.2f} {3:.2f} {4:.2f} {5:.2f} {6:.2f}").format( time.time() - now, (last_x), gyro_total_x, (last_x), (last_y), gyro_total_y, (last_y))
+    return (last_x, last_y, gyro_offset_x, gyro_offset_y, gyro_total_x, gyro_total_y)
+    
+#(gyro_scaled_x, gyro_scaled_y, gyro_scaled_z,
+#accel_scaled_x, accel_scaled_y, accel_scaled_z) = read_all()
+       
+
+    
+bus = smbus.SMBus(1)  # or bus = smbus.SMBus(1) for Revision 2 boards
 # Now wake the 6050 up as it starts in sleep mode
 bus.write_byte_data(address, power_mgmt_1, 0)
 
 now = time.time()
-
 K = 0.98
 K1 = 1 - K
-
 time_diff = 0.01
 
-(gyro_scaled_x, gyro_scaled_y, gyro_scaled_z, accel_scaled_x, accel_scaled_y, accel_scaled_z) = read_all()
-
-last_x = get_x_rotation(accel_scaled_x, accel_scaled_y, accel_scaled_z)
-last_y = get_y_rotation(accel_scaled_x, accel_scaled_y, accel_scaled_z)
-
-gyro_offset_x = gyro_scaled_x 
-gyro_offset_y = gyro_scaled_y
-
-gyro_total_x = (last_x) - gyro_offset_x
-gyro_total_y = (last_y) - gyro_offset_y
-
-print ("{0:.4f} {1:.2f} {2:.2f} {3:.2f} {4:.2f} {5:.2f} {6:.2f}").format( time.time() - now, (last_x), gyro_total_x, (last_x), (last_y), gyro_total_y, (last_y))
-
+(last_x, last_y, gyro_offset_x, gyro_offset_y, gyro_total_x, gyro_total_y) = initPlane()
+    
 
 while True:
     #Lists holding filtered values over time
     rep_x = list()
     rep_y = list()
     #int / time_diff = samples
-    for i in range(0, int(1.5 / time_diff)):
+    for i in range(0, int(3 / time_diff)):
         time.sleep(time_diff - 0.005) 
         
         (gyro_scaled_x, gyro_scaled_y, gyro_scaled_z, accel_scaled_x, accel_scaled_y, accel_scaled_z) = read_all()
@@ -101,5 +108,20 @@ while True:
         rep_y.append(last_y)
 
     #Calculates whether still
+    total = 0
+    for q in range(0,len(rep_x)):
+        try:
+            #May want one axis to be more valuable?
+            #i.e. weight down x movements if y is more important
+            total += (rep_x[q+1] - rep_x[q]) + (rep_y[q+1] - rep_y[q])
+        except IndexError:
+            total += 0
+
+    print(total)
+    #CHANGEME:
+    #Needs testing to determine apropriate value
+    if total > 10:
+        print("Rep!")
     
+        
         
