d051f705ca077d2a0c3ce2b2721df78e3ba1a054
abtheo <c.theo.kent@gmail.com>
2017-05-01 16:24:48 +0100

    Output updated

--- Pi/Output.py
+++ Pi/Output.py
@@ -71,35 +71,34 @@ gyro_total_y = (last_y) - gyro_offset_y
 
 print ("{0:.4f} {1:.2f} {2:.2f} {3:.2f} {4:.2f} {5:.2f} {6:.2f}").format( time.time() - now, (last_x), gyro_total_x, (last_x), (last_y), gyro_total_y, (last_y))
 
-
-while True:
-    #Lists holding filtered values over time
-    rep_x = list()
-    rep_y = list()
-    #int / time_diff = samples
-    for i in range(0, int(1.5 / time_diff)):
-        time.sleep(time_diff - 0.005) 
-        
-        (gyro_scaled_x, gyro_scaled_y, gyro_scaled_z, accel_scaled_x, accel_scaled_y, accel_scaled_z) = read_all()
-        
-        gyro_scaled_x -= gyro_offset_x
-        gyro_scaled_y -= gyro_offset_y
-        
-        gyro_x_delta = (gyro_scaled_x * time_diff)
-        gyro_y_delta = (gyro_scaled_y * time_diff)
-
-        gyro_total_x += gyro_x_delta
-        gyro_total_y += gyro_y_delta
-
-        rotation_x = get_x_rotation(accel_scaled_x, accel_scaled_y, accel_scaled_z)
-        rotation_y = get_y_rotation(accel_scaled_x, accel_scaled_y, accel_scaled_z)
-
-        last_x = K * (last_x + gyro_x_delta) + (K1 * rotation_x)
-        last_y = K * (last_y + gyro_y_delta) + (K1 * rotation_y)
-        
-        rep_x.append(last_x)
-        rep_y.append(last_y)
-
-    #Calculates whether still
+output = list()
+#x / time_diff = samples
+for i in range(0, int(6 / time_diff)):
+    time.sleep(time_diff - 0.005) 
     
-        
+    (gyro_scaled_x, gyro_scaled_y, gyro_scaled_z, accel_scaled_x, accel_scaled_y, accel_scaled_z) = read_all()
+    
+    gyro_scaled_x -= gyro_offset_x
+    gyro_scaled_y -= gyro_offset_y
+    
+    gyro_x_delta = (gyro_scaled_x * time_diff)
+    gyro_y_delta = (gyro_scaled_y * time_diff)
+
+    gyro_total_x += gyro_x_delta
+    gyro_total_y += gyro_y_delta
+
+    rotation_x = get_x_rotation(accel_scaled_x, accel_scaled_y, accel_scaled_z)
+    rotation_y = get_y_rotation(accel_scaled_x, accel_scaled_y, accel_scaled_z)
+
+    last_x = K * (last_x + gyro_x_delta) + (K1 * rotation_x)
+    last_y = K * (last_y + gyro_y_delta) + (K1 * rotation_y)
+    outVar = ( "time:{0:.4f} rot_x:{1:.2f} gyro_x:{2:.2f} last_x:{3:.2f} rot_y:{4:.2f} gyro_x:{5:.2f} last_y:{6:.2f} \n".format( time.time() - now, (rotation_x), (gyro_total_x), (last_x), (rotation_y), (gyro_total_y), (last_y)) )
+    print(outVar)
+    output.append(outVar)
+    
+for x in output:
+    with open("output_new.txt", "a") as myfile:
+        myfile.write(x)
+
+
+    
