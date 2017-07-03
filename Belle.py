4e09d1b55fe1fc1e85d8a81cb903ada64389952a
abtheo <c.theo.kent@gmail.com>
2017-07-03 19:13:22 +0000

    App working!

--- Belle.py
+++ Belle.py
@@ -238,7 +238,7 @@ except KeyboardInterrupt:
 
     ##    ##-----------Framework----------------
     #Outputs to new JSON post each time
-    jsonString = '[{"dumbbell_id":"58dfca3f1d3448278f2fc68a","workout":"test1","reps":10,"form":20}]'
+   # jsonString = '[{"dumbbell_id":"58dfca3f1d3448278f2fc68a","workout":"test1","reps":10,"form":20}]'
     #HTTP Post request on z
     #head = {'Content-Type': 'application/json'}
     #r = requests.post('http://46.101.3.244:8080/api/workoutData', data = jsonString, headers=head)
@@ -246,4 +246,4 @@ except KeyboardInterrupt:
     #print r
 
 
-    os.system('python2.7 Server_test.py ' + json.dumps(jsonString))
+    os.system('python2.7 Server_test.py "58dfca3f1d3448278fc68a" "button" 10 20' )
--- Server_test.py
+++ Server_test.py
@@ -3,11 +3,9 @@ import json
 import sys
 
 
-jsonString = '[{"dumbell_id": "58dfca3f1d3448278f2fc68a","workout": "Pi Test","reps": 80,"form": 97}]'
+jsonString = '[{"dumbell_id": "58dfca3f1d3448278f2fc68a","workout":' + '"' + sys.argv[2] + '"' + ',"reps":' + sys.argv[3] + ',"form":' + sys.argv[4] + '}]'
 #HTTP Post request on z
 head = {'Content-Type': 'application/json'}
-formatter = ("'" + str(sys.argv[1]) + "'")
-print formatter
-print formatter == jsonString
-r = requests.post('http://46.101.3.244:8080/api/workoutData', data = formatter, headers=head)
+print jsonString
+r = requests.post('http://46.101.3.244:8080/api/workoutData', data = jsonString, headers=head)
 print(r)
